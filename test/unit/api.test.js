7ba85b05898158b7162596e6c70b1e651acaf754
Danilo Yorinori <daniloksy@gmail.com>
2023-08-12 22:40:40 -0300

    code of live

---
+++ test/unit/api.test.js
@@ -0,0 +1,73 @@
+import { describe, it } from 'node:test'
+import { handler } from '../../api.js'
+import assert from 'node:assert'
+import { EventEmitter } from 'node:events'
+
+const mockRequest = ({ url, method, headers, body }) => {
+  const options = {
+    url: url ?? '/',
+    method: method ?? 'GET',
+    headers: headers ?? {},
+  }
+
+  const request = new EventEmitter()
+
+  request.url = options.url
+  request.method = options.method
+  request.headers = options.headers
+
+  setTimeout(() => request.emit('data', JSON.stringify(body)))
+
+  return request
+}
+
+const mockResponse = ({ mockContext }) => {
+  const response = {
+    writeHead: mockContext.fn(),
+    end: mockContext.fn(),
+  }
+
+  return response
+}
+
+const getFirstCallArg = (mock) => mock.calls[0].arguments[0]
+
+describe('API Unit test Suite', () => {
+  let _globalToken = ''
+
+  describe('/login', () => {
+    it('should receive not authorized when user or password is invalid', async (context) => {
+      const inputRequest = mockRequest({
+        url: '/login',
+        method: 'POST',
+        body: {
+          user: '',
+          password: '123'
+        }
+      })
+      const outputResponse = mockResponse({
+        mockContext: context.mock
+      })
+
+      await handler(inputRequest, outputResponse)
+
+      // console.log(
+      //   getFirstCallArg(outputResponse.writeHead.mock)
+      // )
+
+      assert.strictEqual(
+        getFirstCallArg(outputResponse.writeHead.mock),
+        401,
+        `should receive 401 status code, received ${getFirstCallArg(outputResponse.writeHead.mock)}`
+      )
+
+      const expectedResponse = JSON.stringify({ error: 'user invalid!' })
+      assert.strictEqual(outputResponse.end.mock.callCount(), 1, 'should call response.end once')
+      assert.deepStrictEqual(
+        getFirstCallArg(outputResponse.end.mock),
+        expectedResponse,
+        `should receive ${JSON.stringify(expectedResponse)}, actual: ${getFirstCallArg(outputResponse.end.mock)}`
+      )
+    })
+  })
+})
